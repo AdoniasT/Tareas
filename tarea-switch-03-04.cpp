69694371e413836767fb16cc75a96ef20cacbcdd
AdoniasT <adoniastojin543@gmail.com>
2024-04-03 08:58:42 -0600

    tarea de menu

---
+++ tarea-switch-03-04.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+    switch(num){
+        case 1: 
+        cout <<"area comun";
+        break;
+        case 3: 
+        cout <<"area comun";
+        break;
+        case 5:
+        cout <<"area especifica";
+        break;
+        case 7:
+        cout <<"area especifica";
+        break;
+        default: 
+        cout << "no nos encontramos en ese semestre";
+    }
+}
